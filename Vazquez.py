b39205d9aba52cf7beaf4d67153bb80825e1af57
Yanhao Li <yanhao.li@outlook.com>
2023-02-17 03:13:47 +0100

    add 3 baseliens

---
+++ Vazquez.py
@@ -0,0 +1,197 @@
+import numpy as np
+from skimage.util.shape import view_as_blocks
+import os
+import glob
+import cv2
+import matplotlib.pyplot as plt
+import mplcursors
+# epsilon = 1e-5
+
+
+class Vazquez:
+    def __init__(self):
+        self.frame_types = None
+        self.stream_nums = None
+        self.display_nums = None
+
+        self.detected_result = {}
+
+        self.S_arr = None
+        self.I_arr = None
+        self.P = None
+
+    def load_residuals(self, fnames, max_num=10000):
+        fnames = np.array(fnames)
+
+        frame_types = np.array([fname.split(".")[-2][-1] for fname in fnames])
+        stream_nums = np.array([int(fname.split("_")[-3][1:]) for fname in fnames])
+        display_nums = np.array([int(fname.split("_")[-2][1:]) for fname in fnames])
+
+        S_arr = []
+        I_arr = []
+        for fname in fnames:
+            img_type = cv2.imread(fname).reshape(-1)
+            num_SMB = (img_type == 3).sum() / 256
+            num_IMB = (img_type == 1).sum() / 256
+            S_arr.append(num_SMB)
+            I_arr.append(num_IMB)
+        self.S_arr = np.array(S_arr)
+        self.I_arr = np.array(I_arr)
+
+        sorted_indices = np.argsort(display_nums)
+
+        self.display_nums = display_nums[sorted_indices]
+        self.frame_types = frame_types[sorted_indices]
+        self.stream_nums = stream_nums[sorted_indices]
+        self.I_arr = self.I_arr[sorted_indices]
+        self.S_arr = self.S_arr[sorted_indices]
+
+
+    def preprocess(self):
+        for i in range(len(self.frame_types)):
+            if self.frame_types[i] == 'I':
+                if i == 0:
+                    self.I_arr[i] = self.I_arr[i + 1]
+                elif i == len(self.frame_types) - 1:
+                    self.I_arr[i] = self.I_arr[i + 1]
+                else:
+                    self.I_arr[i] = (self.I_arr[i - 1] + self.I_arr[i - 1]) / 2
+
+        P = []
+        for n in range(1, len(self.I_arr) - 1):
+            if self.I_arr[n - 1] < self.I_arr[n] and \
+                    self.I_arr[n + 1] < self.I_arr[n] and \
+                    self.S_arr[n - 1] > self.S_arr[n] and \
+                    self.S_arr[n + 1] > self.S_arr[n]:
+                P.append(n)
+        self.P = np.array(P)
+
+        E_arr = np.zeros(len(self.I_arr))
+        E_arr[1:-1] = np.abs((self.I_arr[1:-1] - self.I_arr[0:-2]) * (self.S_arr[1:-1] - self.S_arr[0:-2])) + \
+            np.abs((self.I_arr[2:] - self.I_arr[1:-1]) * (self.S_arr[2:] - self.S_arr[1:-1]))
+
+        self.V_arr = np.zeros(len(self.I_arr))
+        self.V_arr[P] = E_arr[self.P]
+
+
+    def visualize(self, save_fname=None):
+        fig, ax = plt.subplots(figsize=(20, 5))
+        #
+        colors = []
+        for type in self.frame_types:
+            if type == 'I':
+                colors.append('red')
+            elif type == 'P':
+                colors.append('blue')
+            elif type == 'B':
+                colors.append('green')
+            else:
+                raise Exception(f"Invalid type {type}")
+
+        if len(self.detected_result) > 0:
+            for frame_num in self.detected_result["frame_nums"]:
+                colors[frame_num] = 'cyan'
+
+        # plt.text(x=2, y=self.residuals.max(), s=f"periodicity={p} \noffset={b} \nNFA={NFA}",
+        #          horizontalalignment='center',
+        #          verticalalignment='center',
+        #          ha='left', va='top')
+
+        display_nums = np.arange(len(self.V_arr))
+        bars = ax.bar(display_nums, self.V_arr, color=colors)
+
+
+        plt.xlabel('frame number i')
+        plt.ylabel('v(i)')
+
+        cursor = mplcursors.cursor(hover=mplcursors.HoverMode.Transient)
+        @cursor.connect("add")
+        def on_add(sel):
+            x, y, width, height = sel.artist[sel.index].get_bbox().bounds
+            sel.annotation.set(text=f"num={round(x)}, v(i)={height}",
+                               position=(0, 20), anncoords="offset points")
+            sel.annotation.xy = (x, y + height)
+
+        if save_fname is not None:
+            plt.savefig(save_fname, bbox_inches='tight')
+
+        plt.show()
+
+    def estimate_GOP(self):
+        # P = np.where(self.S_PRED > 0)[0]
+        # T = len(self.S_PRED)
+
+        C = set()
+        for i in range(len(self.P)):
+            n1 = self.P[i]
+            for j in range(i + 1, len(self.P)):
+                n2 = self.P[j]
+                c = np.gcd(n1, n2)
+                if c >= 2 and c < len(self.P):
+                    C.add(c)
+        print("C:", C)
+        C = np.array(list(C))
+        phi_arr = []
+        for i in range(len(C)):
+            c = C[i]
+            phi = self.compute_phi(c)
+            phi_arr.append(phi)
+        idx_best = np.argmax(phi_arr)
+
+        phi_max = phi_arr[idx_best]
+
+        T_phi = 0 # how to set? TODO
+        if phi_max > T_phi:
+            G1 = C[idx_best]
+            self.detected_result["G1"] = G1
+        else:
+            G1 = None
+
+        return G1
+
+    def compute_phi(self, c):
+        indices = np.arange(0, len(self.V_arr), c)
+
+        phi1 = self.V_arr[np.intersect1d(indices, self.P)].mean()
+
+        beta = 0.1 * np.max(self.V_arr)
+        phi2 = len(np.setdiff1d(indices, self.P)) * beta
+
+        energy_arr = []
+        for z in range(1, c):
+            energy = self.V_arr[::z].mean()
+            energy_arr.append(energy)
+        phi3 = np.max(np.array(energy_arr))
+
+        return phi1 - phi2 - phi3
+
+
+        # C = np.array(list(C))
+        # phi_arr = np.zeros_like(C).astype(float)
+        #
+        # for i in range(len(C)):
+        #     c = C[i]
+        #     res = compute_phi(self.S_PRED, c, P)
+        #     phi_arr[i] = res
+        #
+        # GOP = C[np.argmax(phi_arr)]
+        # print("The estimated GOP is", GOP)
+
+
+# def compute_phi()
+
+
+def test():
+    root = "/Users/yli/phd/video_processing/jm_16.1/bin"
+    fnames = glob.glob(os.path.join(root, "imgMB*.png"))
+
+    analyzer = Vazquez()
+    analyzer.load_residuals(fnames, max_num=1000)
+    analyzer.preprocess()
+    # analyzer.visualize()
+    GOP = analyzer.estimate_GOP()
+    print("GOP:", GOP)
+
+
+if __name__ == "__main__":
+    test()
---
+++ Yao.py
@@ -0,0 +1,160 @@
+import subprocess
+import numpy as np
+import os
+import glob
+import matplotlib.pyplot as plt
+import mplcursors
+import cv2
+
+
+class YAO:
+    def __init__(self):
+        self.frame_types = None
+        self.stream_nums = None
+        self.display_nums = None
+        self.detected_result = {}
+
+        self.SODBs = None
+
+    def load_SODB(self, vid_fname):
+        """
+        :param vid_fname: input video filename
+        :return: a list of packet sizes in bytes
+        """
+        command = f"ffprobe -show_frames {vid_fname} | grep pkt_size"
+
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        result = result.stdout
+        pkt_sizes = [int(s.split("=")[-1]) for s in result.split("\n") if s != ""]
+        print("pkt_sizes:", pkt_sizes)
+        # print("len pkt_sizes:", len(pkt_sizes))
+
+        self.SODBs = np.array(pkt_sizes)
+
+    def load_frame_info(self, fnames, max_num=10000):
+        fnames = np.array(fnames)
+
+        self.frame_types = np.array([fname.split(".")[-2][-1] for fname in fnames])
+        self.stream_nums = np.array([int(fname.split("_")[-3][1:]) for fname in fnames])
+        self.display_nums = np.array([int(fname.split("_")[-2][1:]) for fname in fnames])
+
+        SMB_features = []
+        for fname in fnames:
+            SMB_feature = 1 / (cv2.imread(fname).mean() + 1e-5)
+            SMB_features.append(SMB_feature)
+        self.SMB_features = np.array(SMB_features)
+
+        sorted_ind = np.argsort(self.display_nums)
+
+        self.frame_types = self.frame_types[sorted_ind][:max_num]
+        self.stream_nums = self.stream_nums[sorted_ind][:max_num]
+        self.display_nums = self.display_nums[sorted_ind][:max_num]
+        self.SODBs = self.SODBs[sorted_ind][:max_num]
+        self.SMB_features = self.SMB_features[sorted_ind][:max_num]
+
+
+    def visualize(self, save_fname=None):
+        fig, ax = plt.subplots(figsize=(20, 5))
+        #
+        colors = []
+        for type in self.frame_types:
+            if type == 'I':
+                colors.append('red')
+            elif type == 'P':
+                colors.append('blue')
+            elif type == 'B':
+                colors.append('green')
+            else:
+                raise Exception(f"Invalid type {type}")
+        print("colors", colors)
+
+        if len(self.detected_result) > 0:
+            for frame_num in self.detected_result["frame_nums"]:
+                colors[frame_num] = 'cyan'
+
+        # plt.text(x=2, y=self.residuals.max(), s=f"periodicity={p} \noffset={b} \nNFA={NFA}",
+        #          horizontalalignment='center',
+        #          verticalalignment='center',
+        #          ha='left', va='top')
+
+        display_nums = np.arange(len(self.SODBs))
+        bars = ax.bar(display_nums, self.Et_features, color=colors)
+
+
+        plt.xlabel('frame number')
+        plt.ylabel('String of data bytes')
+        # plt.title('Sr')
+
+        cursor = mplcursors.cursor(hover=mplcursors.HoverMode.Transient)
+        @cursor.connect("add")
+        def on_add(sel):
+            x, y, width, height = sel.artist[sel.index].get_bbox().bounds
+            sel.annotation.set(text=f"num={round(x)}, SODB={height}",
+                               position=(0, 20), anncoords="offset points")
+            sel.annotation.xy = (x, y + height)
+
+        if save_fname is not None:
+            plt.savefig(save_fname, bbox_inches='tight')
+
+        plt.show()
+
+    def preprocess(self):
+        I_indices = np.where(self.frame_types == 'I')[0]
+        for i in I_indices:
+            if i == 0:
+                self.SODBs[i] = self.SODBs[i+1]
+            elif i == len(self.SODBs):
+                self.SODBs[i] = self.SODBs[i - 1]
+            else:
+                self.SODBs[i] = (self.SODBs[i - 1] + self.SODBs[i + 1]) / 2
+        print(I_indices)
+
+        self.Et_features = self.SODBs * self.SMB_features
+        self.Et_features[I_indices] = self.SODBs[I_indices] / self.SMB_features[I_indices]
+
+    def detect(self):
+        T = len(self.Et_features)
+        G_max = min(150, T // 10)
+        G_candidates = np.arange(2, G_max+1)
+
+        def Lambda(G):
+            return self.Et_features[::G].mean()
+
+        Lambda_candidates = [Lambda(m) for m in G_candidates]
+
+        for i in range(len(G_candidates)):
+            print(f"G={G_candidates[i]}, Lambda={Lambda_candidates[i]}")
+
+        idx_sorted = np.argsort(Lambda_candidates)
+        idx_max1, idx_max2 = idx_sorted[-1], idx_sorted[-2]
+
+        self.T_Lambda = 0.01 # ?
+        if Lambda_candidates[idx_max1] - Lambda_candidates[idx_max2] > self.T_Lambda:
+            G1 = G_candidates[idx_max1]
+            self.detected_result["frame_nums"] = np.arange(0, len(self.Et_features), G1)
+            return G1
+        else:
+            return None
+
+
+
+
+if __name__ == '__main__':
+    # vid_fname = "/Users/yli/phd/deepfake/collection_deepfake/tom_cruise/tom_2.h264"
+    vid_fname = "/Users/yli/phd/deepfake/collection_deepfake/real/office_c2.h264"
+
+    root = "/Users/yli/phd/video_processing/jm_16.1/bin"
+    fnames = glob.glob(os.path.join(root, "imgSB_*.png"))
+
+    analyzer = YAO()
+    analyzer.load_SODB(vid_fname)
+    analyzer.load_frame_info(fnames)
+    analyzer.preprocess()
+
+
+    analyzer.visualize()
+
+    G1 = analyzer.detect()
+    print("Detected G1 = ", G1)
+
+    analyzer.visualize()
