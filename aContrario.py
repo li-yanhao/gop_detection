720507c0bb479bf89bac7a9b8e73b98c842a8b3c
Yanhao Li <yanhao.li@outlook.com>
2023-03-08 01:46:55 +0100

    save

--- aContrario.py
+++ aContrario.py
@@ -305,7 +305,23 @@ class StreamAnalyzer:
             ]
         )
 
+        # fig.update_layout(
+            # autosize=False,
+            # height=500,
+            # margin=dict(
+            #     l=50,
+            #     r=50,
+            #     b=100,
+            #     t=100,
+            #     pad=4
+            # ),
+            # paper_bgcolor="LightSteelBlue",
+        # )
+
         fig.update_layout(
+            autosize=False,
+            # width=500,
+            height=500,
             # font_family="Courier New",
             # font_color="blue",
             # title_font_family="Times New Roman",
